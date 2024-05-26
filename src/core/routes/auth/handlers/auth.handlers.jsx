95a6811123b5db9c4e0856d1417977187f790590
Juan Manuel Espiño <juanm1ta47@gmail.com>
2024-05-26 05:37:07 +0200

    updates

--- src/core/routes/auth/handlers/auth.handlers.jsx
+++ src/core/routes/auth/handlers/auth.handlers.jsx
@@ -16,23 +16,9 @@ const verifyAuthHandler = async ({ setUser, setLoading }) => {
   }
 };
 
-const logoutHandler = async ({ setUser }) => {
-  try {
-    await axios.post("/auth/logout", {}, {
-      withCredentials: true,
-    });
-    setUser(false);
-    // Opcionalmente, redirigir al usuario después del logout
-    window.location.href = '/login';
-  } catch (error) {
-    console.error("Error al cerrar sesión:", error);
-  }
-};
-
 const AuthHandlers = ({ setUser, setLoading }) => {
   return {
     handlerVerifyAuth: () => verifyAuthHandler({ setUser, setLoading }),
-    handlerLogout: () => logoutHandler({ setUser }), // Añadido handler para el logout
   };
 };
 
